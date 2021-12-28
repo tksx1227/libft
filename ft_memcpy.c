126eb0a08e561ff064bcbe1da982f1a3fea5ca9f
tomori <tmrtkm1227tksx@gmail.com>
2021-12-29 00:26:58 +0900

    [fix] Typo of header name

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,21 +6,22 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 18:52:08 by ttomori           #+#    #+#             */
-/*   Updated: 2021/12/28 18:59:07 by ttomori          ###   ########.fr       */
+/*   Updated: 2021/12/29 00:26:00 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ftlib.h"
+#include "libft.h"
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	int	i;
+	char		*p1;
+	const char	*p2;
 
-	i = 0;
-	while (i < n)
+	p1 = (char *)dest;
+	p2 = (const char *)src;
+	while (n-- > 0)
 	{
-		dest[i] = src[i];
-		i++;
+		*(p1 + n) = *(p2 + n);
 	}
 	return (dest);
 }
