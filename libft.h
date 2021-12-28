6a7cf4971f6d1ea7abd8eca406ac0039a6b584fe
tomori <tmrtkm1227tksx@gmail.com>
2021-12-28 18:33:36 +0900

    [update] Add stdio.h

--- libft.h
+++ libft.h
@@ -6,13 +6,14 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 17:51:07 by ttomori           #+#    #+#             */
-/*   Updated: 2021/12/28 17:56:07 by ttomori          ###   ########.fr       */
+/*   Updated: 2021/12/28 18:18:54 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <stdio.h>
 # include <string.h>
 
 void	*ft_memset(void *s, int c, size_t n);
