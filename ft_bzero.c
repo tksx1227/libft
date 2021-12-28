82c8f241270b515a9308a26cf64a6e0903ae5826
tomori <tmrtkm1227tksx@gmail.com>
2021-12-28 23:58:57 +0900

    [update] Add main function

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 17:51:55 by ttomori           #+#    #+#             */
-/*   Updated: 2021/12/28 23:55:33 by ttomori          ###   ########.fr       */
+/*   Updated: 2021/12/28 23:58:35 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,3 +20,16 @@ void	ft_bzero(void *s, size_t n)
 	while (n-- > 0)
 		*(p + n) = '\0';
 }
+
+/*
+int	main(void)
+{
+	char	s[] = "0123456789";
+
+	printf("Original: %s\n", s);
+	ft_bzero(s, 3);
+	printf("Shift 2 : %s\n", s + 2);
+	printf("Shift 3 : %s\n", s + 3);
+	return (0);
+}
+*/
