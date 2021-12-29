312ec499e54fd681c582145f13fe1c1c50dee231
tomori <tmrtkm1227tksx@gmail.com>
2021-12-29 17:09:23 +0900

    [add] Prototype ft_memccpy

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 17:51:07 by ttomori           #+#    #+#             */
-/*   Updated: 2021/12/29 00:49:10 by ttomori          ###   ########.fr       */
+/*   Updated: 2021/12/29 16:12:37 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-size_t	strlen(const char *s);
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+size_t	ft_strlen(const char *s);
 
 #endif
