aaff6f7eb7160a4d05d01064d3e9871df8b551b4
tomori <tmrtkm1227tksx@gmail.com>
2021-12-29 22:24:15 +0900

    [add] Prototype ft_tolower

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 17:51:07 by ttomori           #+#    #+#             */
-/*   Updated: 2021/12/29 22:18:08 by ttomori          ###   ########.fr       */
+/*   Updated: 2021/12/29 22:19:51 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include <string.h>
 
 int	ft_toupper(char c);
+int	ft_tolower(char c);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
