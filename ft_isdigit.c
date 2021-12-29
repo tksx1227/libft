9d4757bc1a012779318215935648eea4bce738b2
tomori <tmrtkm1227tksx@gmail.com>
2021-12-30 00:44:25 +0900

    [fix] Function name in comment

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/29 22:35:48 by ttomori           #+#    #+#             */
-/*   Updated: 2021/12/30 00:07:33 by ttomori          ###   ########.fr       */
+/*   Updated: 2021/12/30 00:43:59 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	main(void)
 	printf("isdigit(%c) = 0x%04x\n", c1, isdigit(c1));
 	printf("isdigit(%c) = 0x%04x\n", c2, isdigit(c2));
 	
-	printf("---------- ft_isalpha ----------\n");
+	printf("---------- ft_isdigit ----------\n");
 	printf("ft_isdigit(%c) = 0x%04x\n", c1, ft_isdigit(c1));
 	printf("ft_isdigit(%c) = 0x%04x\n", c2, ft_isdigit(c2));
 	return (0);
