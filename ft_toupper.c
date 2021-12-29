9e06042e0de76fa1c1f0bd9be06ce94a895be64d
tomori <tmrtkm1227tksx@gmail.com>
2021-12-29 22:19:19 +0900

    [add] ft_toupper function

---
+++ ft_toupper.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/29 22:10:03 by ttomori           #+#    #+#             */
+/*   Updated: 2021/12/29 22:19:01 by ttomori          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_toupper(char c)
+{
+	if ('a' <= c && c <= 'z')
+		return (c - 32);
+	return (c);
+}
+
+/*
+int	main(void)
+{
+	// Parameters
+	char c1 = 'g';
+	char c2 = 'R';
+
+	printf("---------- toupper ----------\n");
+	printf("toupper(%c) = %c\n", c1, toupper(c1));
+	printf("toupper(%c) = %c\n", c2, toupper(c2));
+
+	printf("---------- ft_toupper ----------\n");
+	printf("ft_toupper(%c) = %c\n", c1, ft_toupper(c1));
+	printf("ft_toupper(%c) = %c\n", c2, ft_toupper(c2));
+	return (0);
+}
+*/
