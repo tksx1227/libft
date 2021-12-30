64140999743cfc7439a59e6e0f6ac3b9d3172325
tomori <tmrtkm1227tksx@gmail.com>
2021-12-30 18:32:32 +0900

    [add] Prototype ft_strncpy

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 17:51:07 by ttomori           #+#    #+#             */
-/*   Updated: 2021/12/30 18:06:28 by ttomori          ###   ########.fr       */
+/*   Updated: 2021/12/30 18:17:02 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 char	*ft_strdup(const char *s);
 char	*ft_strcpy(char *dest, const char *src);
+char	*ft_strncpy(char *dest, const char *src, size_t n);
 size_t	ft_strlen(const char *s);
 
 #endif
