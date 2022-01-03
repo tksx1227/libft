19eaf6f18154ac62331d8f7f792b606eda5eaa8a
tomori <tmrtkm1227tksx@gmail.com>
2022-01-03 22:11:00 +0900

    [fix] Type of ft_putchar

--- ft_putchar.c
+++ ft_putchar.c
@@ -6,16 +6,15 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 12:20:26 by ttomori           #+#    #+#             */
-/*   Updated: 2022/01/03 12:40:17 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/01/03 22:09:46 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_putchar(int c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
-	return (c);
 }
 
 /*
@@ -27,15 +26,13 @@ int	main(void)
 
 	printf("---------- putchar ----------\n");
 	printf("putchar(%d) & putchar(%d)\n", c1, c2);
-	int res1 = putchar(c1);
-	int res2 = putchar(c2);
-	printf("Result1 = %d, Result2 = %d\n", res1, res2);
+	putchar(c1);
+	putchar(c2);
 
 	printf("---------- ft_putchar ----------\n");
 	printf("ft_putchar(%d) & ft_putchar(%d)\n", c1, c2);
-	int res3 = ft_putchar(c1);
-	int res4 = ft_putchar(c2);
-	printf("Result1 = %d, Result2 = %d\n", res3, res4);
+	ft_putchar(c1);
+	ft_putchar(c2);
 	return (0);
 }
 */
