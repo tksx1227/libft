406e168e69f82d1aa0c2497f29e9ba58bdca2a98
tomori <tmrtkm1227tksx@gmail.com>
2022-01-10 00:06:53 +0900

    [add] Prototype ft_lstlast

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 17:51:07 by ttomori           #+#    #+#             */
-/*   Updated: 2022/01/09 19:58:45 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/01/09 23:52:45 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,5 +68,6 @@ void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif
