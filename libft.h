15a5c589213cc6e6ce9a39d9e60e5ffef60f7429
tomori <tmrtkm1227tksx@gmail.com>
2022-01-09 23:50:48 +0900

    [add] Prototype ft_lstsize

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 17:51:07 by ttomori           #+#    #+#             */
-/*   Updated: 2022/01/09 19:19:42 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/01/09 19:58:45 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,5 +67,6 @@ void	ft_putnbr_fd(int n, int fd);
 // Bonus part
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
 
 #endif
