14dc0e329007ef4a68d7757c206df97ef9a2139f
tomori <tmrtkm1227tksx@gmail.com>
2022-01-09 19:17:17 +0900

    [add] ft_lstnew function

---
+++ ft_lstnew.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/09 19:09:50 by ttomori           #+#    #+#             */
+/*   Updated: 2022/01/09 19:16:51 by ttomori          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*p;
+
+	p = (t_list *)malloc(sizeof(t_list));
+	if (p == NULL)
+		return (NULL);
+	p->content = content;
+	p->next = NULL;
+	return (p);
+}
+
+/*
+int	main(void)
+{
+	char s[] = "Hello.";
+
+	t_list *res = ft_lstnew(s);
+	printf("Content: \"%s\"\n", (char *)res->content);
+	printf("Next   : %p\n", res->next);
+	free(res);
+
+	return (0);
+}
+*/
