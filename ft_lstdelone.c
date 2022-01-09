89b4f6071efd068b87a00c0aa0e9f95233d6d2c6
tomori <tmrtkm1227tksx@gmail.com>
2022-01-10 01:28:22 +0900

    [add] ft_lstdelone function

---
+++ ft_lstdelone.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/10 01:19:22 by ttomori           #+#    #+#             */
+/*   Updated: 2022/01/10 01:23:59 by ttomori          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (lst == NULL)
+		return ;
+	if (del != NULL)
+		del(lst->content);
+	free(lst);
+}
