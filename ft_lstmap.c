d4b37ea77c1de6b98787897be6d29e05464875f9
tomori <tmrtkm1227tksx@gmail.com>
2022-01-10 11:53:25 +0900

    [add] ft_lstmap function

---
+++ ft_lstmap.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/10 11:29:53 by ttomori           #+#    #+#             */
+/*   Updated: 2022/01/10 11:53:05 by ttomori          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*p;
+	t_list	**pp;
+
+	if (f == NULL)
+		return (NULL);
+	pp = &p;
+	while (lst != NULL)
+	{
+		p = ft_lstnew(f(lst->content));
+		if (p == NULL)
+			return (NULL);
+		if (del != NULL)
+			del(lst->content);
+		lst = lst->next;
+		p = p->next;
+	}
+	return (*pp);
+}
