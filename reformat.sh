d9f8422670cc23d2dcc72b087b1af3c4be2c4b43
tomori <tmrtkm1227tksx@gmail.com>
2022-01-14 00:14:37 +0900

    [update] Reformat file

--- reformat.sh
+++ reformat.sh
@@ -1,6 +1,6 @@
 # Reformat the directory for submission
 mv ./srcs/*.c ./
-mv ./includes/libft.h ./
+mv ./includes/*.h ./
 rm -rf srcs includes .git
 rm Makefile README.md .gitignore
 mv Makefile_submit Makefile
