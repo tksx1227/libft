32354059b24c53825898051d9c11b6a6e4c87e61
tomori <tmrtkm1227tksx@gmail.com>
2022-01-14 13:56:14 +0900

    [add] main function

--- srcs/ft_lstclear_bonus.c
+++ srcs/ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 10:52:22 by ttomori           #+#    #+#             */
-/*   Updated: 2022/01/14 13:32:53 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/01/14 13:56:03 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,3 +28,38 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	}
 	*lst = NULL;
 }
+
+/*
+int	main(void)
+{
+	t_list *lst1 = ft_lstnew("AAA");
+	t_list *lst2 = ft_lstnew("BBB");
+	t_list *lst3 = ft_lstnew("CCC");
+
+	ft_lstadd_back(&lst1, lst2);
+	ft_lstadd_back(&lst1, lst3);
+
+	printf("----- Do not use heap area -----\n");
+	printf("Free linear list\n");
+	ft_lstclear(&lst1, NULL);
+	printf("Done...\n");
+
+	t_list *lst4 = ft_lstnew(ft_strdup("AAA"));
+	t_list *lst5 = ft_lstnew(ft_strdup("BBB"));
+	t_list *lst6 = ft_lstnew(ft_strdup("CCC"));
+
+	ft_lstadd_back(&lst4, lst5);
+	ft_lstadd_back(&lst4, lst6);
+
+	printf("----- Use heap area -----\n");
+	printf("Free linear list\n");
+	ft_lstclear(&lst4, free);
+	printf("Done...\n");
+
+	printf("----- First arg is NULL -----\n");
+	ft_lstclear(NULL, free);
+	printf("Done...\n");
+
+	return (0);
+}
+*/
