4987e560e3cbae310b92d0690c0ca38d44890f83
tomori <tmrtkm1227tksx@gmail.com>
2022-01-14 13:47:13 +0900

    [add] main function

--- srcs/ft_lstdelone_bonus.c
+++ srcs/ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 01:19:22 by ttomori           #+#    #+#             */
-/*   Updated: 2022/01/14 00:54:44 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/01/14 13:46:55 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,3 +20,20 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 		del(lst->content);
 	free(lst);
 }
+
+/*
+int	main(void)
+{
+	t_list *lst1 = ft_lstnew("AAA");
+	t_list *lst2 = ft_lstnew(ft_strdup("BBB"));
+
+	printf("lst1's content: %s\n", (char *)lst1->content);
+	printf("lst2's content: %s\n", (char *)lst2->content);
+
+	printf("Free heap area...\n");
+	ft_lstdelone(lst1, NULL);
+	ft_lstdelone(lst2, free);
+
+	return (0);
+}
+*/
