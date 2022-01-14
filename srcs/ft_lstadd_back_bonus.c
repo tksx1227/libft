103b62c117e7f0bde00f850f59c02d6521badc70
tomori <tmrtkm1227tksx@gmail.com>
2022-01-14 14:09:08 +0900

    [add] main function

--- srcs/ft_lstadd_back_bonus.c
+++ srcs/ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 00:09:40 by ttomori           #+#    #+#             */
-/*   Updated: 2022/01/14 12:10:19 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/01/14 14:08:49 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,32 +26,31 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 }
 
 /*
+static void	print_list_content(t_list *lst)
+{
+	for (int i = 0; lst != NULL; i++)
+	{
+		printf("[%d] Content: %s\n", i, (char *)lst->content);
+		lst = lst->next;
+	}
+}
+
 int	main(void)
 {
-	t_list *lst1 = ft_lstnew("AAA");
-	t_list *lst2 = ft_lstnew("BBB");
-	t_list *lst3 = ft_lstnew("CCC");
-	t_list *temp;
-	t_list **pp;
-	t_list *p;
+	t_list *lst1 = ft_lstnew("HEAD");
+	t_list *lst2 = ft_lstnew("AAA");
+	t_list *lst3 = ft_lstnew("BBB");
+	t_list *lst4 = ft_lstnew("TAIL");
 
-	pp = &lst1;
-	ft_lstadd_back(pp, lst2);
-	ft_lstadd_back(pp, lst3);
-	p = *pp;
-	for (int i = 0; p != NULL; i++)
-	{
-		printf("%d. Content: \"%s\"\n", i, (char *)p->content);
-		p = p->next;
-	}
+	ft_lstadd_back(&lst1, lst2);
+	ft_lstadd_back(&lst1, lst3);
+	ft_lstadd_back(&lst1, lst4);
+	ft_lstadd_back(&lst1, NULL);
+	ft_lstadd_back(&lst1, NULL);
 
-	p = *pp;
-	while (p != NULL)
-	{
-		temp = p->next;
-		free(p);
-		p = temp;
-	}
+	print_list_content(lst1);
+
+	ft_lstclear(&lst1, NULL);
 
 	return (0);
 }
