c707e2d25380ccbb9489ab4a01c9281e864b3e2d
tomori <tmrtkm1227tksx@gmail.com>
2022-01-16 02:13:51 +0900

    [refactor] Reduce redundancy

--- srcs/ft_itoa.c
+++ srcs/ft_itoa.c
@@ -6,53 +6,30 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/09 12:55:15 by ttomori           #+#    #+#             */
-/*   Updated: 2022/01/12 17:29:27 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/01/16 02:12:41 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-static void	reverse_str(char *s)
-{
-	char	temp;
-	size_t	i;
-	size_t	len;
-
-	i = 0;
-	len = ft_strlen(s);
-	while (i < len / 2)
-	{
-		temp = s[i];
-		s[i] = s[len - i - 1];
-		s[len - i - 1] = temp;
-		i++;
-	}
-}
+#include "../includes/libft.h"
 
 static void	convert_recursive(char *s, long long int n)
 {
 	if (n == 0)
-	{
-		*s = '\0';
 		return ;
-	}
-	convert_recursive(s + 1, n / 10);
+	convert_recursive(s - 1, n / 10);
 	*s = (n % 10) + '0';
 }
 
-size_t	count_digit(long long int n)
+int	count_digit(long long int n)
 {
-	size_t	counter;
+	int	counter;
 
-	if (n == 0)
-		return (1);
-	if (n < 0)
+	counter = 0;
+	if (n <= 0)
 	{
 		n = -n;
-		counter = 1;
+		counter++;
 	}
-	else
-		counter = 0;
 	while (n != 0)
 	{
 		counter++;
@@ -63,15 +40,18 @@ size_t	count_digit(long long int n)
 
 char	*ft_itoa(int n)
 {
+	int				digit;
 	char			*p;
 	char			*head;
 	long long int	nbr;
 
 	nbr = (long long int)n;
-	p = (char *)malloc(sizeof(char) * (count_digit(nbr) + 1));
+	digit = count_digit(nbr);
+	p = (char *)malloc(sizeof(char) * (digit + 1));
 	if (p == NULL)
 		return (NULL);
 	head = p;
+	p[digit] = '\0';
 	if (nbr < 0)
 	{
 		*p++ = '-';
@@ -81,8 +61,7 @@ char	*ft_itoa(int n)
 	{
 		*p++ = '0';
 	}
-	convert_recursive(p, nbr);
-	reverse_str(p);
+	convert_recursive(head + digit - 1, nbr);
 	return (head);
 }
 
@@ -98,7 +77,7 @@ int	main(void)
 {
 	// Parameters
 	int n1 = 0;
-	int n2 = 42;
+	int n2 = -42;
 	int n3 = 2147483647;
 	int n4 = -2147483648;
 	int n5 = 12345;
