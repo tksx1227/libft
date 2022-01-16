2eb45fbe9172a7182cdc16974530c7982f1f1cdc
tomori <tmrtkm1227tksx@gmail.com>
2022-01-16 14:05:03 +0900

    [fix] Change type int to size_t

--- srcs/ft_strtrim.c
+++ srcs/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/09 00:59:49 by ttomori           #+#    #+#             */
-/*   Updated: 2022/01/14 16:34:15 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/01/16 14:04:17 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 static int	is_included(const char *s, char c)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
 	while (s[i] != '\0')
@@ -26,45 +26,25 @@ static int	is_included(const char *s, char c)
 	return (0);
 }
 
-static char	*ft_strndup(const char *s, size_t n)
-{
-	size_t	i;
-	size_t	len;
-	char	*p;
-
-	len = ft_strlen(s);
-	if (len < n)
-		n = len;
-	p = (char *)malloc(sizeof(char) * (n + 1));
-	if (p == NULL)
-		return (NULL);
-	i = 0;
-	while (i < n)
-	{
-		p[i] = s[i];
-		i++;
-	}
-	p[i] = '\0';
-	return (p);
-}
-
 char	*ft_strtrim(char const *s, char const *set)
 {
-	int		start_idx;
-	int		end_idx;
-	size_t	len;
 	char	*p;
+	size_t	len;
+	size_t	end_idx;
+	size_t	start_idx;
 
 	if (s == NULL || set == NULL)
 		return (NULL);
-	len = ft_strlen(s);
 	start_idx = 0;
+	len = ft_strlen(s);
+	if (len == 0)
+		return (ft_strdup(""));
 	while (is_included(set, s[start_idx]))
 		start_idx++;
 	end_idx = len - 1;
 	while (start_idx < end_idx && is_included(set, s[end_idx]))
 		end_idx--;
-	p = ft_strndup(s + start_idx, end_idx - start_idx + 1);
+	p = ft_substr(s, (unsigned int)start_idx, end_idx - start_idx + 1);
 	if (p == NULL)
 		return (NULL);
 	return (p);
