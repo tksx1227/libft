3e49c1a6495ed4cb8abb10c9a07a851fe37426b9
tomori <tmrtkm1227tksx@gmail.com>
2022-01-16 19:13:05 +0900

    [refactor] Use ft_memmove

--- srcs/ft_substr.c
+++ srcs/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 22:26:02 by ttomori           #+#    #+#             */
-/*   Updated: 2022/01/14 16:32:20 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/01/16 19:12:24 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,23 @@
 
 char	*ft_substr(const char *s, unsigned int start, size_t maxlen)
 {
-	size_t	i;
+	void	*p;
 	size_t	len;
 	size_t	size;
-	char	*p;
 
 	if (s == NULL)
 		return (NULL);
 	len = ft_strlen(s);
-	if (len < start)
+	if (len < (size_t)start)
 		start = len;
 	size = ft_strlen(&s[start]);
 	if (maxlen < size)
 		size = maxlen;
-	p = (char *)malloc(sizeof(char) * (size + 1));
+	p = ft_calloc(sizeof(char), (size + 1));
 	if (p == NULL)
 		return (NULL);
-	i = 0;
-	while (i < size)
-	{
-		p[i] = s[start + i];
-		i++;
-	}
-	p[i] = '\0';
-	return (p);
+	ft_memmove(p, (void *)(s + start), size);
+	return ((char *)p);
 }
 
 /*
@@ -54,11 +47,16 @@ int	main(void)
 	printf("ft_substr(\"%s\", %d, %ld) = \"%s\"\n", s, start1, maxlen1, res1);
 	char *res2 = ft_substr(s, start1, maxlen2);
 	printf("ft_substr(\"%s\", %d, %ld) = \"%s\"\n", s, start1, maxlen2, res2);
+	free(res1);
+	free(res2);
 
 	char *res3 = ft_substr(s, start2, maxlen1);
 	printf("ft_substr(\"%s\", %d, %ld) = \"%s\"\n", s, start2, maxlen1, res3);
 	char *res4 = ft_substr(s, start2, maxlen2);
 	printf("ft_substr(\"%s\", %d, %ld) = \"%s\"\n", s, start2, maxlen2, res4);
+	free(res3);
+	free(res4);
+
 	return (0);
 }
 */
