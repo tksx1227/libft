6473c2f99572fec2f593f477616cdee3bb1ac429
tomori <tmrtkm1227tksx@gmail.com>
2022-01-20 00:08:14 +0900

    [remove] Extra files

--- reformat.sh
+++
@@ -1,7 +0,0 @@
-# Reformat the directory for submission
-mv ./srcs/*.c ./
-mv ./includes/*.h ./
-rm -rf srcs includes .git
-rm Makefile README.md .gitignore
-mv Makefile_submit Makefile
-rm reformat.sh
