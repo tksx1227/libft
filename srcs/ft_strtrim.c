15d65568318cab4f9443267f71e1aff10ed5d360
tomori <tmrtkm1227tksx@gmail.com>
2022-01-20 00:17:38 +0900

    [refactor] Remove ft_substr_temp

--- srcs/ft_strtrim.c
+++ srcs/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/09 00:59:49 by ttomori           #+#    #+#             */
-/*   Updated: 2022/01/20 00:07:12 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/01/20 00:17:30 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,27 +24,6 @@ static int	is_included(const char *s, char c)
 		i++;
 	}
 	return (0);
-}
-
-static char	*ft_substr_temp(const char *s, size_t start, size_t maxlen)
-{
-	char	*p;
-	size_t	len;
-	size_t	size;
-
-	if (s == NULL)
-		return (NULL);
-	len = ft_strlen(s);
-	if (len <= start)
-		return (ft_strdup(""));
-	size = ft_strlen(s + start);
-	if (maxlen < size)
-		size = maxlen;
-	p = (char *)ft_calloc(size + 1, sizeof(char));
-	if (p == NULL)
-		return (NULL);
-	ft_memmove(p, s + start, size);
-	return (p);
 }
 
 char	*ft_strtrim(char const *s, char const *set)
@@ -65,7 +44,7 @@ char	*ft_strtrim(char const *s, char const *set)
 	end_idx = len - 1;
 	while (start_idx < end_idx && is_included(set, s[end_idx]))
 		end_idx--;
-	p = ft_substr_temp(s, start_idx, end_idx - start_idx + 1);
+	p = ft_substr(s, start_idx, end_idx - start_idx + 1);
 	if (p == NULL)
 		return (NULL);
 	return (p);
